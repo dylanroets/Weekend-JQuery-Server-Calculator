0ce876f41778596f21c712ab1272518ea02b40e3
Dylan Roets <dylanroets@yahoo.com>
2022-11-05 16:14:54 -0500

    able to get function working to grab the math signs individually

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,17 @@
 $(document).ready(handleReady);
 
+let globalSign = ''
+
 function handleReady() {
     console.log('JS and JQuery working');
 
+    $('.math-btns').on('click', grabSign);
+
+}
+
+
+function grabSign() {
+    let thisSign = $(this).text();
+    globalSign = thisSign;
+    console.log('Math Sign: ', globalSign)
 }
