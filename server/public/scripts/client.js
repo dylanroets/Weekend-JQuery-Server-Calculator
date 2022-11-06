95fd9ad7eafde529d6bb4ae99bc124f744935a31
Dylan Roets <dylanroets@yahoo.com>
2022-11-05 19:28:37 -0500

    able to send over problem to server, and solve using a switch

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,17 +1,42 @@
 $(document).ready(handleReady);
 
-let globalSign = ''
+let operator = 0
+let number1 = 0
+let number2 = 0
 
 function handleReady() {
     console.log('JS and JQuery working');
-
+    // Grabs input numbers
+    $('#equal-btn').on('click', sendEquation)
+    // Grabs math sign
     $('.math-btns').on('click', grabSign);
+    // Clears the input fields
+    $('#clear-btn').on('click', clearFields);
 
 }
 
-
+function sendEquation() {
+    console.log('in sendEquation');
+    $.ajax({
+        method: 'POST',
+        url: '/sending-equation',
+        data: {
+            num1: $('#num1-input').val(),
+            operator: operator,
+            num2: $('#num2-input').val()
+        }
+    }).then (function(response){
+        console.log('equation sending: ', response);
+    }).catch (function(error){
+        alert('Error Sending Equation: ', error)
+    })
+}
 function grabSign() {
     let thisSign = $(this).text();
-    globalSign = thisSign;
-    console.log('Math Sign: ', globalSign)
+    operator = thisSign;
+    console.log('Math Sign: ', operator)
+}
+
+function clearFields(){
+    $('.inputs').val('');
 }
--- server/server.js
+++ server/server.js
@@ -10,10 +10,49 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET and POST routes go here!
+// GET Equation Route
+app.post('/sending-equation', (req, res) => {
+    let equationObj = req.body;
+    prevEquation.push(equationObj);
+    console.log('Equation posted: ', equationObj);
+    console.log('Previous Equations', prevEquation);
+    console.log('sign received', equationObj.operator);
+    res.sendStatus(200);
+    switch(equationObj.operator) {
+        case '+':
+            equationObj.answer = Number(+equationObj.num1 + +equationObj.num2);
+            break;
+        case '-':
+            equationObj.answer = Number(+equationObj.num1 - +equationObj.num2);
+            break;
+        case '*':
+            equationObj.answer = Number(+equationObj.num1 * +equationObj.num2);
+            break;
+        case '/':
+            equationObj.answer = Number(+equationObj.num1 / +equationObj.num2);
+            break;
+    }
+    console.log('switch result: ', equationObj.answer);
 
 
 
+})
 
+//Array for all previous equations
+let prevEquation = []
+// testing equations history
+
+// switch(equationObj.operator) {
+//     case "+":
+//         return equationObj.num1 + equationObj.num2;
+//     case "-":
+//         return equationObj.num1 - equationObj.num2;
+//     case "*":
+//         return equationObj.num1 * equationObj.num2;
+//     case "/":
+//         return equationObj.num1 / equationObj.num2;
+// }
+// console.log('switch result: ', equationObj);
 
 
 
